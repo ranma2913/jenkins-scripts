6107f3dcb38253a110ce8f5a1fe3f7c44c1d25b0
Joel Sticha (jsticha) <joel.sticha@optum.com>
2020-09-01 08:45:25 -0500

    Initial work on Jenkins CLI setup script

---
+++ Setup-Update_JenkinsCli.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+echo "This script installs or updates the Jenkins CLI"
+read -p "Are you sure you want to continue? (y/n): " -n 1 -r IS_CONTINUE
+function enableXtrace() {
+  set -o xtrace
+}
+function disableXtrace() {
+  set +o xtrace
+}
+
+if [[ $IS_CONTINUE =~ ^[Yy]$ ]]; then
+  enableXtrace
+  DOWNLOAD_CLI_JAR=false
+  if test -f 'jenkins-cli.jar'; then
+    read -p "Do you want to update the jenkins-cli.jar? (y/n): " -n 1 -r IS_CONTINUE
+    if [[ $IS_CONTINUE =~ ^[Yy]$ ]]; then
+      rm 'jenkins-cli.jar'
+      DOWNLOAD_CLI_JAR=true
+    fi
+  else
+    DOWNLOAD_CLI_JAR=true
+  fi
+
+  if [ "$DOWNLOAD_CLI_JAR" = true ]; then
+    read -p 'Please enter your jenkins url. (ex. https://riptide-jenkins-cloud.optum.com/): ' JENKINS_URL
+    JENKINS_URL=${JENKINS_URL:-https://riptide-jenkins-cloud.optum.com/}
+
+    JENKINS_PORT=443
+    KEYSTOREFILE=jenkinsKeyStore
+    KEYSTOREPASS=changeme
+
+    # Initialize an empty keystore
+    keytool -genkeypair -alias boguscert -storepass storePassword -keypass $KEYSTOREPASS -keystore $KEYSTOREFILE -dname "CN=Developer, OU=Department, O=Company, L=City, ST=State, C=CA"
+    keytool -delete -alias boguscert -storepass storePassword -keystore $KEYSTOREFILE
+    
+    # get the SSL certificate
+    wget https://repo1.uhc.com/artifactory/UHG-certificates/optum/Optum_Root_CA.cer
+
+    # create a keystore and import certificate
+    keytool -import -noprompt -trustcacerts -alias Optum_Root_CA -file Optum_Root_CA.cer -keystore ${KEYSTOREFILE} -storepass ${KEYSTOREPASS}
+
+    # verify that the certificate is listed
+    keytool -list -v -keystore ${KEYSTOREFILE} -storepass ${KEYSTOREPASS}
+
+    # get jenkins-cli
+    wget --no-check-certificate ${JENKINS_URL}/jnlpJars/jenkins-cli.jar
+
+    echo '' >>dotfiles/.jenkins-cli
+    ALIAS_COMMAND=$("java -Djavax.net.ssl.trustStore=${KEYSTOREFILE} -Djavax.net.ssl.trustStorePassword=${KEYSTOREPASS} -jar jenkins-cli.jar -s \${JENKINS_URL}")
+    echo "alias jcli=$ALIAS_COMMAND" >>dotfiles/.jenkins-cli
+  fi
+
+  JENKINS_SCRIPTS_HOME=$(pwd)
+
+  echo "Update .bash_profile with Jenkins CLI info"
+  BASH_PROFILE=~/.bash_profile
+  touch $BASH_PROFILE
+  ADD_SOURCE=false
+  grep -qxF '# import `~/.jenkins-cli` if it exists' $BASH_PROFILE || ADD_SOURCE=true
+  if [ "$ADD_SOURCE" = true ]; then
+    echo '' >>$BASH_PROFILE
+    echo '#########################################' >>$BASH_PROFILE
+    echo '# Jenkins CLI' >>$BASH_PROFILE
+    echo "export JENKINS_SCRIPTS_HOME=$JENKINS_SCRIPTS_HOME" >>$BASH_PROFILE
+    echo 'export PATH="$JENKINS_SCRIPTS_HOME:$PATH"' >>$BASH_PROFILE
+    echo '# import `~/.jenkins-cli` if it exists' >>$BASH_PROFILE
+    echo 'if [ -f $JENKINS_SCRIPTS_HOME/dotfiles/.jenkins-cli ]; then' >>$BASH_PROFILE
+    echo '  source $JENKINS_SCRIPTS_HOME/dotfiles/.jenkins-cli' >>$BASH_PROFILE
+    echo 'fi' >>$BASH_PROFILE
+    echo '' >>$BASH_PROFILE
+  fi
+fi
