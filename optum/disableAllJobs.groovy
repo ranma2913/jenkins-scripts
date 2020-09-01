31787c93a0a6a1fbc04ed43ea303ec0bbddf7b18
Joel Sticha (jsticha) <4295880+ranma2913@users.noreply.github.com>
2020-09-01 10:29:23 -0500

    Add gradle build

---
+++ optum/disableAllJobs.groovy
@@ -0,0 +1,19 @@
+import hudson.model.*
+
+disableChildren(Hudson.instance.items)
+
+def disableChildren(items) {
+    for (item in items) {
+        if (item.class.canonicalName == 'com.cloudbees.hudson.plugins.folder.Folder') {
+            disableChildren(((com.cloudbees.hudson.plugins.folder.Folder) item).getItems())
+        } else if (item.class.canonicalName != 'org.jenkinsci.plugins.workflow.job.WorkflowJob') {
+            try {
+                println(item.name)
+                item.disabled = true
+                item.save()
+            } catch (e) {
+                println(e)
+            }
+        }
+    }
+}
