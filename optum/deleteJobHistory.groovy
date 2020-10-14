f073d1c9c117035777c5fd943ecfcd04a67ae775
Joel Sticha (jsticha) <joel.sticha@optum.com>
2020-10-14 13:27:36 -0500

    Add deleteJobHistory.groovy

--- optum/deleteJobHistory.groovy
+++ optum/deleteJobHistory.groovy
@@ -2,14 +2,18 @@ import jenkins.model.Jenkins
 import hudson.model.Job
 
 MAX_BUILDS = 5
-
 Jenkins.instance.getAllItems(Job.class).each { job ->
     println job.name
     def recent = job.builds.limit(MAX_BUILDS)
     for (build in job.builds) {
         if (!recent.contains(build)) {
-            println "Preparing to delete: " + build
-            build.delete()
+            println "Preparing to delete: $build"
+            try{
+                build.delete()
+            }catch(e){
+                println "Unable to delete build: $build"
+                println e
+            }
         }
     }
 }
