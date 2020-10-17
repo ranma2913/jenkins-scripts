4d75d0f4e0e55906089b05e4681e61a10e69b31c
Joel Sticha (jsticha) <joel.sticha@optum.com>
2020-10-16 21:42:27 -0500

    jcli working

---
+++ optum/delete_artifacts.groovy
@@ -0,0 +1,33 @@
+import hudson.*
+import hudson.model.*
+import jenkins.*
+import jenkins.model.*
+import org.jenkinsci.plugins.workflow.job.*
+
+// Delete old artifacts that fills up the disk on the master node.
+// Run this from the Jenkins console (Manage Jenkins, Manage Nodes, master, Script Console)
+
+//def itemNameMatcher = "/"
+def itemNameMatcher = "Fortify_Scan/covid-portal/"
+//def itemNameMatcher = "Fortify_Scan/"
+def numBuildsKeepArtifacts = 1
+
+def totalSize = 0
+println("************************************************** START ***************************************************************")
+Jenkins.instance.getAllItems(Job.class).each { job ->
+    if ((job.fullName =~ itemNameMatcher).find()) {
+        def recent = job.builds.limit(numBuildsKeepArtifacts)
+        for (build in job.builds) {
+            if (!recent.contains(build)) {
+                def artifacts = build.artifacts
+                artifacts.each { artifact ->
+                    totalSize += artifact.getFileSize()
+                    println("$artifact, ${artifact.getFileSize()}")
+                }
+                build.deleteArtifacts()
+            }
+        }
+    }
+}
+println "Total size: $totalSize"
+return "************************************************* END ******************************************************************"
---
+++ optum/delete_job_history.groovy
@@ -0,0 +1,34 @@
+import hudson.*
+import hudson.model.*
+import jenkins.*
+import jenkins.model.*
+import org.jenkinsci.plugins.workflow.job.*
+
+def MAX_BUILDS = 2
+//def itemNameMatcher = "/"
+def itemNameMatcher = "Fortify_Scan/covid-portal/"
+//def itemNameMatcher = "Fortify_Scan/"
+def jobsToDelete = []
+
+println("************************************************** START ***************************************************************")
+Jenkins.instance.getAllItems(Job.class).each { job ->
+    if ((job.fullName =~ itemNameMatcher).find()) {
+        def recent = job.builds.limit(MAX_BUILDS)
+        for (build in job.builds) {
+            if (!recent.contains(build)) {
+                jobsToDelete.add(build)
+            }
+        }
+    }
+}
+println "${jobsToDelete.size()} Jobs to Delete..."
+jobsToDelete.each() { build ->
+    try {
+        println("Deleting Build: ${build}")
+        build.delete()
+    } catch (e) {
+        println "Unable to delete build: $build"
+        println e
+    }
+}
+return "************************************************* END ******************************************************************"
